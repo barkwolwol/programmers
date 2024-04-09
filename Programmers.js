a3c180bf8463cb5fd1375495a8a522f83f79c823
barkwolwol <barkwolwol@gmail.com>
2024-04-09 13:33:24 +0900

    Update Programmers.js

--- Programmers.js
+++ Programmers.js
@@ -106,6 +106,45 @@ function solution(numbers, n) {
 // 5명씩
 // 최대 5명씩 탑승가능한 놀이기구를 타기 위해 줄을 서있는 사람들의 이름이 담긴 문자열 리스트 names가 주어질 때, 앞에서 부터 5명씩 묶은 그룹의 가장 앞에 서있는 사람들의 이름을 담은 리스트를 return하도록 solution 함수를 완성해주세요. 마지막 그룹이 5명이 되지 않더라도 가장 앞에 있는 사람의 이름을 포함합니다.
 function solution(names) {
-    return names.filter((_, i) => i % 5 === 0)
+    return names.filter((_, i) => i % 5 === 0);
 }
 
+function solution(names) {
+    var answer = [];
+    
+    for(i = 0; i < names.length; i += 5){
+        answer.push(names[i])
+    }
+    
+    return answer;
+}
+
+function solution(names) {
+    var answer = [];
+    
+    for(i = 0; i < names.length; i++){
+        if(i % 5 === 0){
+            answer.push(names[i])    
+        }
+    }
+    
+    return answer;
+}
+
+// 정수 찾기
+// 정수 리스트 num_list와 찾으려는 정수 n이 주어질 때, num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+function solution(num_list, n) {
+    var answer = 0;
+    
+    for(i = 0; i < num_list.length; i++) {
+        if(num_list[i] === n) {
+            answer = 1;
+        }
+    }
+    
+    return answer;
+}
+
+function solution(num_list, n) {
+  return num_list.includes(n) ? 1 : 0;
+}
